668cf5f4ba00b2eedf0b948c16b08490d99d3351
Артем Иванов <IvanovArtyom3000@yandex.ru>
2023-05-24 09:41:18 +0300

    Добавьте файлы проекта.

---
+++ Strip Comments/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+public class StripCommentsSolution
+{
+    public static void Main()
+    {
+        // Test
+        var t = StripComments("apples, pears # and bananas\ngrapes\nbananas !apples", new[] { "#", "!" });
+        // ...should return "apples, pears\ngrapes\nbananas"
+    }
+
+    public static string StripComments(string text, string[] commentSymbols)
+    { 
+        string[] lines = text.Split("\n").Select(str => str.Split(commentSymbols, 0).First().TrimEnd()).ToArray();
+
+        return string.Join("\n", lines);
+    }
+}
